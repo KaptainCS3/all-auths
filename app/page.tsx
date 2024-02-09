c60f039993ae941dc3fd3b805027863d8f9733bb
KaptainCS3 <mbiapplegryn@gmail.com>
2024-02-09 20:09:15 +0100

    inital commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Login from "./ui/login";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     <Login />
+    </main>
+  )
+}
